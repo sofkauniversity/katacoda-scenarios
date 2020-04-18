caa6d83fcc60f8212763236144ac72cb7e568954
Meli <magudelo169@gmail.com>
2020-04-18 18:53:12 -0500

    Auto start editor

--- course-html/scen-html-semantic/assets/wait.sh
+++ course-html/scen-html-semantic/assets/wait.sh
@@ -7,8 +7,9 @@ show_progress()
   local -r delay='0.75'
   local spinstr='\|/-'
   local temp
+  /opt/start-vscode.sh && echo "done" >> /home/katacoda-finished
   while true; do
-    sudo grep -i "done" /root/katacoda-finished &> /dev/null
+    sudo grep -i "done" /home/katacoda-finished &> /dev/null
     if [[ "$?" -ne 0 ]]; then
       temp="${spinstr#?}"
       printf " [%c]  " "${spinstr}"
@@ -24,7 +25,7 @@ show_progress()
   echo "Ambiente iniciado"
   echo -n "Configurando ambiente"
   while true; do
-    sudo grep -i "done" /root/katacoda-background-finished &> /dev/null
+    sudo curl -IL http://localhost:23000 | grep -i "HTTP/1.1 200 OK"  &> /dev/null
     if [[ "$?" -ne 0 ]]; then
       temp="${spinstr#?}"
       printf " [%c]  " "${spinstr}"
