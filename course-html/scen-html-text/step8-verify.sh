06647c1dc7e16e1b3be62e596011745d4991f47a
Meli <magudelo169@gmail.com>
2020-04-19 13:28:23 -0500

    Se estandariza limpiado

--- course-html/scen-html-text/step8-verify.sh
+++ course-html/scen-html-text/step8-verify.sh
@@ -1,2 +1,1 @@
-[ -f /home/scrapbook/tutorial/index.html ] && html-differ /opt/step8.html /home/scrapbook/tutorial/index.html | wc -l | grep "^0" && echo "done"
-> /home/scrapbook/tutorial/index.html
+[ -f /home/scrapbook/tutorial/index.html ] && html-differ /opt/step8.html /home/scrapbook/tutorial/index.html | wc -l | grep "^0" && echo "done" && > /home/scrapbook/tutorial/index.html
