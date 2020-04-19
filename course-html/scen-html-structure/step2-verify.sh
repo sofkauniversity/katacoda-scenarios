2a38f62ee36b94c41af66c60c1146deeb800436a
Meli <magudelo169@gmail.com>
2020-04-19 00:28:44 -0500

    Se finaliza scenario

--- course-html/scen-html-structure/step2-verify.sh
+++ course-html/scen-html-structure/step2-verify.sh
@@ -1,2 +1,2 @@
-> /home/scrapbook/tutorial/index.html
 [ -f /home/scrapbook/tutorial/index.html ] && html-differ /opt/step2.html /home/scrapbook/tutorial/index.html | wc -l | grep 0 && echo "done"
+> /home/scrapbook/tutorial/index.html
