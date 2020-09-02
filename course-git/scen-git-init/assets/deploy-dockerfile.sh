a1e211122234264695e333db7df794cfe41f86ae
mora <>
2020-09-01 22:55:10 -0500

    Update docker

--- course-git/scen-git-init/assets/deploy-dockerfile.sh
+++ course-git/scen-git-init/assets/deploy-dockerfile.sh
@@ -6,5 +6,5 @@ ls /home
 ls /tmp/cv-katacoda
 cp -R /tmp/cv-katacoda/. /home
 ls /home
-yarn install -–offline --frozen-lockfile
+yarn install -–offline --cache-folder /usr/local/share/.cache/yarn/v6
 yarn start
