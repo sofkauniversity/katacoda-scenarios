3d239f4ea37652fb4d2245b4dc02feb1efab0c81
Sofka Auto <manuel.salas@sofka.com.co>
2020-09-30 16:26:39 -0500

    Escenario Git Init Configuración Paso 1

--- course-git/scen-git-config/foreground.sh
+++ course-git/scen-git-config/foreground.sh
@@ -1,2 +1,3 @@
+sleep 3;
 echo Construyendo ambiente para práctica, Disfruta tus misiones y pon en práctica todo lo que vas aprendiendo
 cd /home/scrapbook/tutorial/
--- course-git/scen-git-init/foreground.sh
+++ course-git/scen-git-init/foreground.sh
@@ -1,1 +1,2 @@
+sleep 3;
 cd /home/scrapbook/tutorial/
