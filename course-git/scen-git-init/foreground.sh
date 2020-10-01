166e29ea40f981ba843217c980bd2bcfac851f9b
Sofka Auto <manuel.salas@sofka.com.co>
2020-10-01 09:41:13 -0500

    Solución Eror Servidor

--- course-git/scen-git-init/foreground.sh
+++ course-git/scen-git-init/foreground.sh
@@ -1,3 +1,2 @@
 sleep 3;
 cd /home/scrapbook/tutorial/
-python3 -m http.server 80
---
+++ course-git/scen-git-init/launch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sleep 3;
+sh /opt/run-dockerfile.sh
+git config --global user.name "Curso Protección"
+git config --global user.email cursoproteccion@example.com
+echo finalizó la construcción del ambiente puede iniciar su práctica
