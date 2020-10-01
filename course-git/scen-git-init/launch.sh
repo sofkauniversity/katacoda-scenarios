76129525f8f18ccdd0e4d7de163c8343faf6a2d9
Sofka Auto <manuel.salas@sofka.com.co>
2020-10-01 10:58:22 -0500

    Errores con el Servidor

--- course-git/scen-git-init/launch.sh
+++ course-git/scen-git-init/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sleep 3;
-echo Construyendo ambiente de pruebas, espere hasta que finalice la ejecución ...
-cd /home/scrapbook/tutorial/
-sh /opt/run-dockerfile.sh
-echo finalizó la construcción del ambiente puede iniciar su práctica
+sleep 3;
+echo Construyendo ambiente de pruebas, espere hasta que finalice la ejecución ...
+cd /home/scrapbook/tutorial/
+sh /opt/run-dockerfile.sh
+echo finalizó la construcción del ambiente puede iniciar su práctica
 
