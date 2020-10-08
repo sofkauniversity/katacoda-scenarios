f3d9b6145d15471dc0241eaa66e2f1ce9acc317f
Sofka Auto <manuel.salas@sofka.com.co>
2020-10-08 09:36:54 -0500

    Creación de escernarios git log con comandos

--- course-git/scen-git-log/assets/run-dockerfile.sh
+++ course-git/scen-git-log/assets/run-dockerfile.sh
@@ -1,7 +1,10 @@
 cd /home/scrapbook/tutorial
+sleep 4
 git config --global user.email "cursoproteccion@example.com"
 git config --global user.name "Curso Katacoda"
-python3 -m http.server 80
 git init
+sleep 4
 git add .
 git commit -m "Creación v1.0 del Proyecto Web"
+python3 -m http.server 80
+
--- course-git/scen-git-log/launch.sh
+++ course-git/scen-git-log/launch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sleep 3;
+sleep 4;
 echo Construyendo ambiente de pruebas, espere hasta que finalice la ejecución ...
 cd /home/scrapbook/tutorial/
 sh /opt/run-dockerfile.sh
