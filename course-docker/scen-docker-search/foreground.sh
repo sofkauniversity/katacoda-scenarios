c1a0378844c0b421522a7d7f9f346a6eeddbfd23
Manuel Salas Rivas <manuel.salas@sofka.com.co>
2021-07-11 21:08:16 -0500

    Cambios Archivo launch primer escenario docker

--- course-docker/scen-docker-search/foreground.sh
+++ course-docker/scen-docker-search/foreground.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sleep 3;
-echo Ambiente Construido para tus prácticas en KataCoda. Átrevete, prueba y experimenta, estas en un ambiente seguro de práctica.
+echo -n Ambiente Construido para tus prácticas en KataCoda. Átrevete, prueba y experimenta, estas en un ambiente seguro de práctica.
--- course-docker/scen-docker-search/launch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sleep 3;
-echo Ambiente Construido para tus prácticas en KataCoda. Átrevete, prueba y experimenta, estas en un ambiente seguro de práctica.
-
-
